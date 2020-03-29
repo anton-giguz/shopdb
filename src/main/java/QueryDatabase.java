f33a75645f438bbad279a5599a0941920a8afe1b
anton-giguz <61377952+anton-giguz@users.noreply.github.com>
2020-03-30 06:39:55 +0700

    Search customers by productName and minTimes

--- src/main/java/QueryDatabase.java
+++ src/main/java/QueryDatabase.java
@@ -49,6 +49,24 @@ public class QueryDatabase {
     }
 
 
+    private static String getString(JsonObject item, String key) throws CriteriaException {
+        JsonValue value = item.get(key);
+        if (value == null || value.getValueType() != JsonValue.ValueType.STRING) {
+            throw new CriteriaException("Wrong string format");
+        }
+        return item.getString(key);
+    }
+
+
+    private static int getInt(JsonObject item, String key) throws CriteriaException {
+        JsonValue value = item.get(key);
+        if (value == null || value.getValueType() != JsonValue.ValueType.NUMBER) {
+            throw new CriteriaException("Wrong number format");
+        }
+        return item.getInt(key);
+    }
+
+
     public static void main(String[] args) {
         if (args.length < 2 || !args[0].equals("search")) {
             System.err.println("Usage: java -jar shopdb.jar search <input>.json");
@@ -58,21 +76,35 @@ public class QueryDatabase {
 
         try {
             Connection connection = getConnection();
+
             for (JsonValue criteria : getCriteriaArray(inputJson)) {
+                PreparedStatement ps = null;
                 JsonObject item = getCriteriaItem(criteria);
                 Set<String> keys = item.keySet();
-                if (keys.contains("lastName") && keys.size() == 1) {
-                    String lastName = item.getString("lastName");
-                    PreparedStatement ps = connection.prepareStatement("SELECT first_name FROM customers WHERE last_name = ? ORDER BY first_name, id;");
-                    ps.setString(1, lastName);
 
-                    ResultSet rs = ps.executeQuery();
-                    while (rs.next()) {
-                        String firstName = rs.getString("first_name");
-                        System.out.println(lastName + " " + firstName);
-                    }
+                if (keys.contains("lastName") && keys.size() == 1) {
+                    String lastName = getString(item, "lastName");
+                    ps = connection.prepareStatement("SELECT last_name, first_name " +
+                        "FROM customers WHERE last_name = ? ORDER BY last_name, first_name, id;");
+                    ps.setString(1, lastName);
+                } else if (keys.contains("productName") && keys.contains("minTimes") && keys.size() == 2) {
+                    String productName = getString(item, "productName");
+                    int minTimes = getInt(item, "minTimes");
+                    ps = connection.prepareStatement("SELECT last_name, first_name " +
+                        "FROM customers JOIN purchases ON (customers.id = purchases.customer) JOIN products ON (purchases.product = products.id) " +
+                        "WHERE products.name = ? GROUP BY last_name, first_name, customers.id " +
+                        "HAVING count(*) >= ? ORDER BY last_name, first_name, customers.id;");
+                    ps.setString(1, productName);
+                    ps.setInt(2, minTimes);
                 } else {
                     throw new CriteriaException("Unknown criteria");
+                }
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    String lastName  = rs.getString("last_name");
+                    String firstName = rs.getString("first_name");
+                    System.out.println(lastName + " " + firstName);
                 }
             }
         } catch (Exception e) {
